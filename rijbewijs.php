53df08e6cbc15dfcbf0127fa8202227563a65f32
Timothy Kroon <timothykroon1@gmail.com>
2020-03-03 14:00:04 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,10 @@
+<?php
+    echo "hoe oud ben je?" .  PHP_EOL;
+    $leeftijd = readline();
+    $requirmend = 16.5;
+
+    if ($leeftijd > $requirmend){
+        echo "je mag beginnen met rijlessen";
+    } else{
+        echo "helaas, je  mag nog niet beginnen met je rijlessen";
+    }
